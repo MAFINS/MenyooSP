bd6c8f954267301c5736c5b5f6b223147bc90457
Leve <30744982+Leve1@users.noreply.github.com>
2019-09-24 23:37:56 -0600

    Update main.h

--- Solution/source/main.h
+++ Solution/source/main.h
@@ -212,6 +212,9 @@ enum eGameVersion : int
 	
         VER_1_0_1737_0_STEAM,
         VER_1_0_1737_0_NOSTEAM,
+	
+	VER_1_0_1737_6_STEAM,
+        VER_1_0_1737_6_NOSTEAM,
 
 	VER_SIZE,
 	VER_UNK = -1
