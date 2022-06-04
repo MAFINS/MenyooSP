d2ddf5d24965645324ede7074602e669a9fc876c
MAFINS <mafintsxbox@live.com>
2022-06-04 14:36:57 +1000

    Update missing gameversion number

--- Solution/source/main.h
+++ Solution/source/main.h
@@ -246,6 +246,9 @@ enum eGameVersion : int
 
 	VER_1_0_2612_0_STEAM,
 	VER_1_0_2612_0_NOSTEAM,
+	
+        VER_1_0_2628_0_STEAM,
+        VER_1_0_2628_0_NOSTEAM,
 
 	VER_1_0_2628_2_STEAM,
 	VER_1_0_2628_2_NOSTEAM,
