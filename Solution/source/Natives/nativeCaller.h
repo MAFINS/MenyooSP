7ca729ecbd36a10415b03858f6024fb1d8d07f88
maf <mafintsxbox@live.com>
2022-10-13 11:53:21 +1100

    Re-add compile time static_asserts for nativePush and invoke

--- Solution/source/Natives/nativeCaller.h
+++ Solution/source/Natives/nativeCaller.h
@@ -13,7 +13,7 @@ template <typename T>
 static inline void nativePush(T value)
 {
 	UINT64 val64 = 0;
-	//static_assert(sizeof(T) <= sizeof(UINT64), "error, value size > 64 bit");
+	static_assert(sizeof(T) <= sizeof(UINT64), "error, value size > 64 bit");
 	*reinterpret_cast<T *>(&val64) = value; // &val + sizeof(dw) - sizeof(val)
 	nativePush64(val64);
 }
@@ -27,8 +27,8 @@ static inline void nativePush(T value)
 template <typename R, typename... TArgs>
 static inline R invoke(UINT64 hash, TArgs... args)
 {
-	//static_assert(sizeof...(TArgs) <= 25, "Cannot push more than 25 Args to a native");
-	//static_assert(sizeof(R) <= 24, "Natives cannot return data types larger than 24 bytes");
+	static_assert(sizeof...(TArgs) <= 25, "Cannot push more than 25 Args to a native");
+	static_assert(sizeof(R) <= 24, "Natives cannot return data types larger than 24 bytes");
 	nativeInit(hash);
 	(nativePush(args), ...);
 	return *reinterpret_cast<R *>(nativeCall());
@@ -37,7 +37,7 @@ static inline R invoke(UINT64 hash, TArgs... args)
 template <typename R>
 inline R invoke(UINT64 hash)
 {
-	//static_assert(sizeof(R) <= 24, "Natives cannot return data types larger than 24 bytes");
+	static_assert(sizeof(R) <= 24, "Natives cannot return data types larger than 24 bytes");
 	nativeInit(hash);
 	return *reinterpret_cast<R *>(nativeCall());
 }
