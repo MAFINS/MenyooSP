67b943b9a21d721b5b32d2f970bef1105fa25cdb
itsjustcurtis <70447190+itsjustcurtis@users.noreply.github.com>
2022-12-19 22:20:21 +0000

    Potential fix for #424
    Disabled resets the currentop variable, causing the menus to reset to the top when users expect the option to be where they left it. No known consequences in testing are apparent as yet.

--- Solution/source/Menu/Menu.cpp
+++ Solution/source/Menu/Menu.cpp
@@ -590,8 +590,7 @@ void Menu::while_opened()
 	{
 		SetSub_closed();
 	}
-
-
+		
 }
 void Menu::Up(bool playSound)
 {
@@ -639,7 +638,9 @@ void Menu::SetSub_previous()
 	Game::Sound::PlayFrontend_default("BACK"); // Play sound
 
 	*currentopATM = currentop;
+
 }
+
 void Menu::SetSub_new(INT sub_index)
 {
 	currentsub_ar_index++; //Increment array index
@@ -653,7 +654,10 @@ void Menu::SetSub_new(INT sub_index)
 	totalop = 0; // Reset total number of options var"
 
 	*currentopATM = currentop; //SetSub_new complete
+
 }
+
+
 void Menu::SetSub_closed()
 {
 	//Game::RequestScript("cellphone_controller");
@@ -840,8 +844,10 @@ void Menu::sub_handler()
 	{
 		submenu_switch();
 
-		if (Menu::currentop > Menu::printingop) { Menu::currentop = Menu::printingop + 1; Menu::Up(false); }
-		else if (Menu::currentop < 1) { Menu::currentop = 0; Menu::Down(false); }
+		//Does anyone know what the below does? Caused shit when trying to navigate back and retain menu position - IJC
+
+		//if (Menu::currentop > Menu::printingop) { Menu::currentop = Menu::printingop + 1; Menu::Up(false); }
+		//else if (Menu::currentop < 1) { Menu::currentop = 0; Menu::Down(false); }
 
 		//// These czechs is kill
 		//if (currentop < 1) currentop = 1;
