485b25ee3bde65d2650778d332bedf627c3bd7a6
Andy <mail@ak95.de>
2024-01-20 09:00:07 +0100

    move drift category up to cars in WeaponsOptions.cpp List_VehicleCats_Sub

--- Solution/source/Submenus/WeaponOptions.cpp
+++ Solution/source/Submenus/WeaponOptions.cpp
@@ -760,6 +760,7 @@ namespace sub
 		AddvcatOption_("SUVs", Indices::SUV);
 		AddvcatOption_("Sedans", Indices::SEDAN);
 		AddvcatOption_("Compacts", Indices::COMPACT);
+		AddvcatOption_("Drift", Indices::DRIFT);
 
 		AddBreak("---Industrial---");
 		AddvcatOption_("Vans", Indices::VAN);
@@ -774,7 +775,6 @@ namespace sub
 		AddvcatOption_("Military", Indices::MILITARY);
 		AddvcatOption_("Motorcycles", Indices::MOTORCYCLE);
 		AddvcatOption_("Bicycles", Indices::BICYCLE);
-		AddvcatOption_("Drift", Indices::DRIFT);
 		AddvcatOption_("Others", Indices::OTHER);
 
 		/*PCHAR chartick;
